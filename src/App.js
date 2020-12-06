2762160ceb32c8d95607f1f85219c9f8aba7850d
Brooke Kennison <brookelkennison@gmail.com>
2020-12-05 19:48:07 -0500

    style header and main

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header.js'
+import Slogan from './components/Slogan.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Slogan />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Slogan.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../style/slogan.css'
+import '../index.css'
+
+class Slogan extends Component {
+    render() {
+        return (
+            <div className="Slogan">
+                <h1>"Competing with the best to bring you the best"</h1>
+            </div>
+        )
+    }
+}
+
+export default Slogan;
