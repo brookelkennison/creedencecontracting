53bab392b34b6d141622e9243473f28bfdc444b1
Brooke Kennison <brookelkennison@gmail.com>
2020-12-19 11:35:39 -0500

    styled form in contact section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-import Header from './components/Header.js'
-import Slogan from './components/Slogan.js'
-import About from './components/About.js'
-import Services from './components/Services.js'
+import Header from './components/Header.js';
+import Slogan from './components/Slogan.js';
+import About from './components/About.js';
+import Services from './components/Services.js';
+import Contact from './components/Contact.js';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Slogan />
-      <About />
-      <Services />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Header />
+			<Slogan />
+			<About />
+			<Services />
+			<Contact />
+		</div>
+	);
 }
 
 export default App;
