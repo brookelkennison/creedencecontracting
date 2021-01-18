f89177ad430e290bf3f9905e920c500a427f3b53
Brooke Kennison <brookelkennison@gmail.com>
2021-01-17 21:58:20 -0500

    test with template express.js

---
+++ example.js
@@ -0,0 +1,27 @@
+const mailjet = require('node-mailjet').connect('161ba37b8ac65c1be7c6e18c52896f2a', '46b0a816df421be1f62864a34c830072');
+const request = mailjet.post('send', { version: 'v3.1' }).request({
+	Messages: [
+		{
+			From: {
+				Email: 'brookelkennison@gmail.com',
+				Name: 'Brooke',
+			},
+			To: [
+				{
+					Email: 'kennisoncreative@gmail.com',
+					Name: 'Test',
+				},
+			],
+			TemplateID: 2226328,
+			TemplateLanguage: true,
+			Subject: 'Test Email',
+		},
+	],
+});
+request
+	.then((result) => {
+		console.log(result.body);
+	})
+	.catch((err) => {
+		console.log(err.statusCode);
+	});
