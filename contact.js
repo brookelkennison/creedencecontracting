6e0f36c5d153143438424cacbd5b5662e200fd92
Brooke Kennison <brookelkennison@gmail.com>
2021-02-06 15:37:05 -0500

    remove other post requests

--- contact.js
+++ contact.js
@@ -1,35 +1,6 @@
 const mailjet = require('node-mailjet').connect('161ba37b8ac65c1be7c6e18c52896f2a', '46b0a816df421be1f62864a34c830072');
 // const mailjet = require('node-mailjet').connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE);
 
-function addSender(messageInfo) {
-	return mailjet
-		.post('sender', { version: 'v3' })
-		.request({
-			EmailType: 'transactional',
-			IsDefaultSender: 'false',
-			Name: messageInfo.nameContact,
-			Email: messageInfo.email,
-		})
-		.then((result) => {
-			console.log(result.body);
-			validateSender(result.body.Data[0].ID);
-		})
-		.catch((err) => {
-			console.log(err.statusCode);
-		});
-}
-
-function validateSender(senderId) {
-	const request = mailjet.post('sender', { version: 'v3' }).id(senderId).action('validate').request();
-	request
-		.then((result) => {
-			console.log(result.body);
-		})
-		.catch((err) => {
-			console.log(err.statusCode);
-		});
-}
-
 function sendEmail(messageInfo) {
 	return mailjet
 		.post('send', { version: 'v3.1' })
